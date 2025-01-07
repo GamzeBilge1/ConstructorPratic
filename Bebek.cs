a12f0660b35475ab4b1cc7b0cbf2f245dd13206d
Gamze <bilgegamze227@outlook.com>
2025-01-07 16:24:00 +0300

    ..

---
+++ Bebek.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorPratic
+{
+    public class Bebek
+    { 
+        // bebek sınıfının fieldları
+        private string _ad;
+        private string _soyad;
+        private DateTime _dogumTarihi;
+
+        public string Ad // _ad özelliğine diğer sınıflardan erişim sağlamak ve değer atamak için bu property kullanılır.
+        {
+            get { return _ad; }
+            set { _ad = value; }
+        }
+
+        public string Soyad // _soyad özelliğine diğer sınıflardan erişim sağlamak ve değer atamak için bu property kullanılır.
+        {
+            get { return _soyad; }
+            set { _soyad = value; }
+        }
+
+        public DateTime DogumTarihi // _doğumtarihi özelliğine diğer sınıflardan erişim sağlamak ve değer atamak için bu property kullanılır.
+        {
+            get { return _dogumTarihi; }
+            set { _dogumTarihi = value; }
+        }
+        public Bebek()  // Default Constructor (Parametre almayan yapıcı metot)
+       
+        {
+            _dogumTarihi = DateTime.Now;   // Doğum tarihi olarak o anki tarih atanır 
+            Console.WriteLine("Ingaaa");
+
+        }
+
+        // Parametreli Constructor
+        public Bebek(string ad, string soyad)  // Ad ve Soyad parametre olarak alınır ve alanlara atanır
+        {
+            _ad = ad;
+            _soyad = soyad;
+            _dogumTarihi = DateTime.Now;   // Doğum tarihi olarak o anki tarih atanır 
+            Console.WriteLine("Ingaaa");
+        }
+
+        public void Yazdir()  // Yazdir metodu
+        {
+            Console.WriteLine($"Ad: {_ad}");
+            Console.WriteLine($"Soyad: {_soyad}");
+            Console.WriteLine($"Doğum Tarihi: {_dogumTarihi}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+using ConstructorPratic;
+
+Bebek bebek = new Bebek("Efe", "Bilge"); // Parametreli constructor ile bir Bebek nesnesi oluşturuluyor
+
+Console.WriteLine("Bebek Bilgileri:");
+bebek.Yazdir(); // Yazdir metodu ile özellikler ekrana yazdırılıyor
+
+Console.WriteLine("-----------");
+Bebek bebek2 = new Bebek(); // Default constructor ile bir Bebek nesnesi oluşturuluyor
+
+// Ad ve Soyad özellikleri sonradan atanıyor
+bebek2.Ad = "ali";
+bebek2.Soyad= "uzun";
+
+Console.WriteLine("Bebek Bilgileri:");
+bebek2.Yazdir(); // Yazdir metodu ile özellikler ekrana yazdırılıyor
